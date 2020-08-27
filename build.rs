1c82b73d5f87a232dcee9bde5366bc962393a9aa
Wael El Oraiby <wael.eloraiby@gmail.com>
2020-08-26 22:07:01 -0400

    Version 0.1.4: use OUT_DIR

--- build.rs
+++ build.rs
@@ -58,7 +58,7 @@ fn rebuild() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from("src");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
